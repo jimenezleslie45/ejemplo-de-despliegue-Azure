b2403a36ec9d3f9eadf0c819c95f1f86f30c7d8f
jimenezleslie45 <eliasjimenezld@gmail.com>
2026-04-29 23:56:00 -0600

    Actualizando mensaje de bienvenida

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h1>¡Bienvenido a mi App Automatizada leslie GARCIA!</h1><p>Despliegue inicial exitoso.</p>'
+    return '<h1>¡Bienvenido a mi App Automatizada leslie Garcia!</h1><p>Despliegue inicial exitoso.</p>'
 
 if __name__ == '__main__':
     app.run()
